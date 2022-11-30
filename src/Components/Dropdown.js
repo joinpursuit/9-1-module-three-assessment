a69e28406f15f681da6179d9fb66213d0ab48e5b
DestinyJoyner <destinyjoyner@pursuit.org>
2022-11-30 12:35:08 -0500

    movies dropdown display info and reverts when blank, css done

---
+++ src/Components/Dropdown.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Dropdown({dataArr, stateVar, stateVarFunction, displayVar, displayVarFunction }) {
+     // handle onChange function
+     function handleSelect(e) {
+        const value = e.target.value
+        stateVarFunction(value)
+        if(value === ""){
+            displayVarFunction({
+                title: "",
+                releaseDate: "",
+                description: "",
+            })
+        }
+        else{
+            const movie = dataArr.find(({id}) => id === value)
+            displayVarFunction({
+                title: movie.title,
+                releaseDate: movie.release_date,
+                description: movie.description
+            })
+        }
+       
+    }
+  
+    return (
+        <select
+        value = {stateVar}
+        onChange = {(event) => handleSelect(event)}>
+            <option 
+            value = "">
+            </option>
+            { dataArr.length &&
+                dataArr.map(({title,id}) => 
+                <option 
+                value = {id}
+                key = {id}>
+                    {title}
+                </option>
+                )
+            }
+        </select>
+    );
+}
+
+export default Dropdown;
--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { ContextData } from "../Provider";
+import Dropdown from "./Dropdown";
+import './Movies.css'
 
 function Movies(props) {
+    const {movies, setMovies, fetchData } = useContext(ContextData)
+    // declare state for dropdown
+    const [select, setSelect] = useState("")
+    // declare state for object values for select choice
+    const [movieInfo, setMovieInfo] = useState({
+        title: "",
+        releaseDate: "",
+        description: "",
+    })
+   
+
+    useEffect(() => {
+        fetchData(`films`)
+        .then(respJson => setMovies(respJson))
+    })
     return (
-        <div>
-            Movies
+        <div className="movies">
+            <h2>Select A Movie</h2>
+            <Dropdown 
+            dataArr={movies}
+            stateVar = {select} 
+            stateVarFunction = {setSelect}
+            displayVar = {movieInfo}
+            displayVarFunction = {setMovieInfo}
+             />
+                {
+                movieInfo.title && 
+                <section className="movieInfo">
+                    <h4>{movieInfo.title}</h4>
+                    <p>Released: {movieInfo.releaseDate}</p>
+                    <p>Description: {movieInfo.description}</p>
+                </section>
+                    
+                }
+
+             
+            
+            
         </div>
     );
 }
