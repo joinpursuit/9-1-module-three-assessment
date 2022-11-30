6bda4dfdfa2b5aa0e1bdf6eb470a3db960dde8cb
sreeves07 <shamarreeves@pursuit.org>
2022-11-30 11:02:02 -0500

    nav and home page completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
+
 function App() {
   return (
-    <div className="app">
-      <h1>Welcome to GhibliApp</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Nav />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/locations" element={<Locations />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/People.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const People = () => {
+    return (
+        <div>
+            <h1>Search a person</h1>
+        </div>
+    );
+};
+
+export default People;
