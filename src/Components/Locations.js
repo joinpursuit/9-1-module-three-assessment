3f7ec6530d83d1b160b5811caf675a46abd2e872
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-11-30 12:51:36 -0500

    started locations

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,5 +1,20 @@
-export default function Locations () {
-    return (
-        <p>locations</p>
-    )
+import { useState, useEffect } from "react";
+
+export default function Locations() {
+  const [locations, setLocations] = useState([]);
+  const [buttonVal, setButtonVal] = useState("SHOW");
+
+  useEffect(() => {
+    fetch("locations.json")
+      .then((res) => res.json())
+      .then((data) => setLocations(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="locations">
+      <h2>List of Locations</h2>
+      <button>{buttonVal} LOCATIONS</button>
+    </div>
+  );
 }
