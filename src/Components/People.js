e1d12faa5cbf7ba531b5a500350c39a744099a54
sreeves07 <shamarreeves@pursuit.org>
2022-11-30 11:36:18 -0500

    people completed

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const People = () => {
+    const [people, setPeople] = useState([])
+    const [input, setInput] = useState({})
+    const [name, setName] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const found = people.find(item => item.name.toLowerCase() === name.toLowerCase())
+        setInput(found || {})
+    }
+
+    useEffect(()=>{
+        fetch('/people.json')
+            .then((res)=>res.json())
+            .then((data)=>{
+                setPeople(data)
+                // console.log(data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            }) 
+    },[])
+
     return (
-        <div>
-            <h1>Search a person</h1>
+        <div className='.people'>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='person'>
+                    <input 
+                    type="text" 
+                    placeholder="Haku, Pazu..." 
+                    value={name} 
+                    onChange={(e)=>{setName(e.target.value)}}/>
+                </label>
+                    <button type="submit">Submit</button>
+                </form>
+                {input.id && (
+                    <div>
+                       <h1>Name: {input.name}</h1>
+                        <p>Age: {input.age}</p>
+                        <p>Gender: {input.gender}</p>
+                    </div>
+                )}
         </div>
     );
 };
