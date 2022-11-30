7a09dc916b657239e0abdb4a2aa8a3f431c0bb18
D-Stukes <dstukes888@gmail.com>
2022-11-30 11:37:23 -0500

    created Home, Movies, People, Location and Nav components and setup Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+
 function App() {
   return (
     <div className="app">
       <h1>Welcome to GhibliApp</h1>
+      <div> <Nav /> </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/people" element={<People/>} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
