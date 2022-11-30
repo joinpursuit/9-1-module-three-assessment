c6078c972e79f804c88ab1f72a795c0b80a17a75
bowersjuan <bowersjuan@gmail.com>
2022-11-30 12:07:18 -0500

    Create and update Movies component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./common/Navbar";
+import Movies from "./components/Movies";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<h1>Welcome to GhibliApp</h1>} />
-          <Route path="/movies" element={<h1>Movies</h1>} />
+          <Route path="/movies" element={<Movies />} />
           <Route path="/people" element={<h1>People</h1>} />
           <Route path="/locations" element={<h1>Locations</h1>} />
         </Routes>
---
+++ src/components/Movies.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+import { get } from "../fetch";
+
+export default function Movies() {
+  const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState({});
+
+  function handleSelect(e) {
+    setMovie(movies.find((movie) => movie.title === e.target.value));
+  }
+
+  useEffect(() => {
+    get("films").then((res) => setMovies(res));
+  });
+
+  return (
+    <div className="movies">
+      <h2>Select a Movie</h2>
+      <label htmlFor="movieSelect">
+        <select id="movieSelect" onChange={handleSelect}>
+          <option value=""></option>
+          {movies.map((movie) => (
+            <option value={movie.title} key={movie.id}>
+              {movie.title}
+            </option>
+          ))}
+        </select>
+      </label>
+      {movie ? "movie chosen" : ""}
+    </div>
+  );
+}
