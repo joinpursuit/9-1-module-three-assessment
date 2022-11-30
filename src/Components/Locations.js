fc91ec0b71a4d0f66500975d890a2db6cf8b3346
sreeves07 <shamarreeves@pursuit.org>
2022-11-30 11:50:04 -0500

    locations showing

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Locations = () => {
+    const [location, setLocation] = useState([])
+    const [showLocations, setShowLocations] = useState(false)
+
+    useEffect(()=>{
+        fetch('/locations.json')
+            .then((res)=>res.json())
+            .then((data)=>{
+                setLocation(data)
+                // console.log(data)
+            })
+            .catch((err)=>{
+                console.log(err)
+            }) 
+    },[])
+
     return (
-        <div>
+        <div className='.locations'>
             <h1>List of Locations</h1>
+            <div className='showButton'>
+                <button onClick={() => {setShowLocations(!showLocations)}}>{!showLocations ? "Show Locations..." : "Hide Locations..."}</button>
+                <ul>
+                    {showLocations && location && location.map((data) => (
+                    <li key={data.id}> Name: {data.name} Climate: {data.climate} Terrain: {data.terrain}</li> 
+                    ))} 
+                </ul>
+            </div>
         </div>
     );
 };
