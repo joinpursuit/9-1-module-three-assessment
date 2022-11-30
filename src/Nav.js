25b500b6851c6770915f60fa96c03c025235e764
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-11-30 11:54:58 -0500

    localhost works

--- src/Nav.js
+++ src/Nav.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default function Nav() {
   return (
-    <Nav>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/movies">Movies</Link>
       <Link to="/people">People</Link>
       <Link to="/location">Location</Link>
-    </Nav>
+    </nav>
   );
 }
