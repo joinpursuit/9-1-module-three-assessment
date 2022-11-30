8606fea1af8350f4dd7d50771010991ce5f8f3a9
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-11-30 12:55:33 -0500

    did till people page

--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,53 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './Movie.css';
 export default function Movies() {
-  return <div>Movies</div>;
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(true);
+  const [single, setSingle] = useState({});
+
+  const handleChoice = (e) => {
+    const chosen = e.target.value;
+    console.log(chosen);
+    const found = data.find((movie) => movie.id === chosen);
+    setSingle(found || {});
+  };
+  useEffect(() => {
+    fetch('/films.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="movies">
+      <h1 className="h1-tag">Select a Movie</h1>
+      <div>
+        <select onChange={handleChoice}>
+          <option value=""></option>
+          {data.map((movie) => (
+            <option key={movie.id} value={movie.id}>
+              {movie.title}
+            </option>
+          ))}
+        </select>
+        {single.id && (
+          <div>
+            <p>
+              <h2>Title: {single.title}</h2>
+            </p>
+            <p>
+              <b>Release Date:</b>
+              {single.release_date}
+            </p>
+            <p>
+              <b>Description:</b>
+              {single.description}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/People.js
+++ src/People.js
@@ -1,5 +1,62 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './People.css';
 export default function People() {
-  return <div>People</div>;
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+  const [input, setInput] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const found = data.find(
+      (person) => person.name.toLowerCase() === name.toLowerCase()
+    );
+    setInput(found || {});
+  };
+
+  useEffect(() => {
+    fetch('/people.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="people">
+      <h2>Search for a Person</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <input
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter a name"
+          />
+        </label>
+        <button type="submit">submit</button>
+      </form>
+      {input.id ? (
+        <div>
+          <h1>Name: {input.name}</h1>
+          <p>
+            <b>Age: </b>
+            {input.age}
+          </p>
+          <p>
+            <b>Eye Color: </b>
+            {input.eye_color}
+          </p>
+          <p>
+            <b>Hair Color: </b>
+            {input.hair_color}
+          </p>
+        </div>
+      ) : (
+        <p>Not Found</p>
+      )}
+    </div>
+  );
 }
