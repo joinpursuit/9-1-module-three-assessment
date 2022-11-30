6e0b0faa2e5fcd33ed0354946b2a00bebdce3e18
bowersjuan <bowersjuan@gmail.com>
2022-11-30 11:13:03 -0500

    Create working routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { get } from "./fetch";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  get("people").then((res) => console.log(res));
   return (
-    <div className="app">
-      <h1>Welcome to GhibliApp</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <nav></nav>
+        <Routes>
+          <Route path="/" element={<h1>Welcome to GhibliApp</h1>} />
+          <Route path="/movies" element={<h1>Movies</h1>} />
+          <Route path="/people" element={<h1>People</h1>} />
+          <Route path="/locations" element={<h1>Locations</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
