48d3f3df0104be3fff087657d145decbe7f43fef
TrianePeart <TrianePeart@gmail.com>
2022-11-30 22:38:29 -0500

    Movies Complete

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+
 
 export default function Movies () {
-    return (
-        <div>
-            <h1>Welcome to Movies</h1>
+    const [datas, setData] = useState([])
+    const [movie, setMovies] = useState({})
+    // ASK HELP: When calling this object movies it was frying the code but the name should not matter. See why this was happening during review 'Movie was movies and it was showing data as a number set' 
+
+    useEffect(() => {
+        fetch('./films.json')
+        .then(res => res.json())
+        .then(data => setData(data))
+        .catch((err) =>{
+            console.log(err)
+        });
+    }, []) 
+
+    function handleChange (e){
+        const found = datas.find(({ id }) => id === e.target.value);
+        setMovies(found || {});
+    };
+
+    return(
+        <div className='movie'>
+            <h2>Select A Movie</h2>
+            <select onChange={handleChange}>
+                <option value=''></option>
+                {datas && datas.map(({id, title}) =>(
+                    <option key={id} value={id}>
+                        {title}
+                    </option>
+                ))}
+            </select>
+
+            {movie.id && (
+                <aside className='movies'>
+                    <h2>
+                        <span>Title: </span> {movie.title}
+                    </h2>
+                    <p>
+                        <span>Release Date: </span> {movie.release_date}
+                    </p>
+                    <p>
+                        <span>Description: </span>{movie.description}
+                    </p>
+                </aside>
+            )}
         </div>
-    );
-};
+    )
+}
+
