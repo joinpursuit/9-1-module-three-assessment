ae68554a3cda3d07fc9ecc9ce116cc4d7a09525f
D-Stukes <dstukes888@gmail.com>
2022-12-01 15:25:45 -0500

    updated Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Welcome to GhibliApp</div>
+    <div>
+        <div className='welcome'>Welcome to GhibliApp</div>
+        <div className='welcomePicsBox'>
+            <div className='welcomePic1'></div>
+            <div className='welcomePic2'></div>
+            <div className='welcomePic3'></div>
+        </div>
+
+
+
+    </div>
   )
 }
