0d77eb6dd37093959ee0b1e221f880c49c240827
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-11-30 19:23:54 -0500

    got find person returning person data and error message for 0length input box

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,26 +1,50 @@
 import { useEffect, useState } from "react";
 import "./Movies.css";
 const Movies = () => {
-  const URL = `../public/movies.JSON`;
   const [data, setData] = useState([]);
+  const [filmInfo, setFilmInfo] = useState([]);
 
   useEffect(() => {
-    fetch(URL)
+    fetch("../films.json")
       .then((res) => res.json())
-      .then((resJson) => setData(resJson))
+      .then((resJson) => setData(resJson || []))
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    const choice = e.target.value;
+    const foundFilm = data.find((film) => film.id === choice);
+    setFilmInfo(foundFilm || []);
+  };
 
   return (
     <div className="movies">
       <h1>Select a Movie</h1>
-      <select className="movies-select" value="">
+      <select className="movies-select" onChange={handleChange}>
         <option value=""></option>
+        {data?.map((film) => (
+          <option key={film.id} value={film.id}>
+            {film.title}
+          </option>
+        ))}
       </select>
+      <hr />
+
+      {filmInfo.length !== 0 ? (
+        <div className="film-info">
+          <h2>{filmInfo.title}</h2>
+          <p>
+            <strong>Release Date:</strong>
+            {filmInfo.release_date}
+          </p>
+          <p>
+            <strong>Description:</strong>
+            {filmInfo.description}
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/People.js
+++ src/components/People.js
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import "./People.css";
+
 const People = () => {
-  return <div className="people">People</div>;
+  const [data, setData] = useState([]);
+  const [personInfo, setPersonInfo] = useState([]);
+  const [inputBox, setInputBox] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch("../people.json")
+      .then((res) => res.json())
+      .then((resJson) => setData(resJson || []))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleInput = (e) => {
+    setInputBox(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(false);
+    if (inputBox.length === 0) setError(true);
+    const foundPerson = data?.find(
+      (person) => person.name.toLowerCase() === inputBox.toLowerCase()
+    );
+    setPersonInfo(foundPerson || []);
+    setInputBox("");
+  };
+
+  return (
+    <div className="people">
+      <h1>Search for a Person</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputBox} onChange={handleInput}></input>
+        <button type="submit">Sumbit</button>
+        <hr />
+        {error ? <div className="error">You must enter a name!</div> : null}
+      </form>
+    </div>
+  );
 };
 
 export default People;
