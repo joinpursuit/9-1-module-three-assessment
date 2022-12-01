dcdcb8f4b9d0361608c81eb5e26c9f553c9a5f10
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-01 10:59:19 -0500

    people section complete

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,28 +1,86 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ContextData } from "../Provider";
 import './People.css'
 
 function People(props) {
     const {people, setPeople, fetchData} = useContext(ContextData)
 
+    // declare state for search input
+    const [searchInput, setSearchInput] = useState("")
+    // declare state for person info
+    const [personInfo, setPersonInfo] = useState({
+        name: "",
+        age: "",
+        gender:"",
+    })
+    // declare state for notFound
+    const [notFound, setNotFound] = useState(false)
+
+    // function onChange for search input
+    function handleTextInput(e) {
+        setSearchInput(e.target.value)
+    }
+    // function to handle form Submit
+    function handleSubmit(e) {
+        e.preventDefault()
+        // turn name value to array use includes to find match if user types in one part of character name (lusheeta => Lusheeta Toel Ul Laputa )
+        const find = people.find(({name}) => name.toLowerCase().split(` `).includes(searchInput.toLowerCase()))
+        
+        if(!find){
+            setPersonInfo({
+                name: "",
+                age: "",
+                gender:"",
+            })
+            setNotFound(true)
+        }
+        else {
+            setNotFound(false)
+            setPersonInfo({
+                name: find.name,
+                age: find.age,
+                gender: find.gender
+            })
+        }
+        setSearchInput("")
+
+    }
+
+
     useEffect(() => {
         fetchData(`people`)
         .then(respJson => setPeople(respJson))
         .catch(err => console.log(err))
-    })
+    }, [])
     return (
         <div className='people'>
             <h2>Search for a Person</h2>
 
-            <form>
+            <form onSubmit={(event)=> handleSubmit(event)} >
                 <input
-                type= "text">
+                type= "text"
+                placeholder="Enter a character name..."
+                value={searchInput}
+                onChange = {(event) => handleTextInput(event)}>
                 </input>
 
                 <button
-                type='button'
+                type='submit'
                 >Submit</button>
             </form>
+
+            <section>
+                {
+                    personInfo.name &&
+                     <div className="characterInfo">
+                        <h3>Name: {personInfo.name}</h3>
+                        <p>Age: {personInfo.age}</p>
+                        <p>{personInfo.gender}</p>
+                    </div> || notFound && <span>Not Found</span>
+                } 
+              
+
+            </section>
         </div>
     );
 }
