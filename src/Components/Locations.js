1649483e690bcba7e6481f4f1108d47f99d89bd5
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-01 12:55:00 -0500

    all sort buttons function

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -7,29 +7,36 @@ function Locations(props) {
     const {locations, setLocations, fetchData} = useContext(ContextData)
     // declare state for show/hide button
     const [clicked, setClicked] =useState(false)
+    // declare state for sorted array
+    const [sorted, setSorted] = useState([])
 
     // function for sorting
-    function sortBy() {
-      const sortArr = locations.map(({name}) => name).sort()
-
-      sortArr.forEach((location, i ) => 
-        sortArr[i] = locations.find(({name}) => name === location)
-      )
-      setLocations(sortArr)
-      
+    function sortBy(value) {
+        // return array of objects with value and idValue key for inputted value
+        const sortArr = locations.map(obj => {
+           return {value: obj[`${value}`], idValue: obj.id}
+            }) 
+        // sort arr of objects by value key alphabetically (comparison using sort 1,-1,0)
+            // a<b -1, a>b 1, else 0
+        sortArr.sort((a,b) => 
+            // if a<b return -1, else return 1 OR return 0
+            a.value < b.value ? -1 : 1 || 0
+        )
+        // for each obj in sortedArr, find & match the idValue to the id value in original locations array and update sortArr's index with the returned matching obj from locations data
+        sortArr.forEach(({idValue}, i ) => 
+          sortArr[i] = locations.find(({id}) => id === idValue)
+        )
+        setSorted(sortArr) 
     }
+
+    let arr = sorted.length ? sorted : locations
     
-
-
     useEffect(()=>{
         fetchData(`locations`)
         .then(respJson => setLocations(respJson))
         .catch(err => console.log(err))
-
-//         const fruits = ["Banana", "Orange", "Apple", "Mango"];
-// console.log(fruits.sort())
-// console.log(fruits.reverse())
     }, [])
+    
     return (
         <div className="locations">
           <h2>List of Locations</h2> 
@@ -40,15 +47,17 @@ function Locations(props) {
             </button>
 
             <button
-            onClick={() => sortBy()}>
+            onClick={() => sortBy(`name`)}>
                 Sort by Name
             </button>
 
-            <button>
+            <button
+            onClick={() => sortBy(`climate`)}>
                 Sort by Climate
             </button>
 
-            <button>
+            <button
+            onClick={() => sortBy(`terrain`)}>
                 Sort by Terrain
             </button>
           </div>
@@ -56,7 +65,7 @@ function Locations(props) {
           <ul className="thumbnailGrid">
             {
                 clicked &&
-                locations.map(({name, climate, terrain, id}) => 
+                arr.map(({name, climate, terrain, id}) => 
                 <LocationThumbnail
                 key = {id} 
                 name = {name}
