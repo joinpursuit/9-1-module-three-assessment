909626fda9a33de19ed531da5a7bcd691f245369
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-01 12:13:41 -0500

    sort by name success

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -8,17 +8,51 @@ function Locations(props) {
     // declare state for show/hide button
     const [clicked, setClicked] =useState(false)
 
+    // function for sorting
+    function sortBy() {
+      const sortArr = locations.map(({name}) => name).sort()
+
+      sortArr.forEach((location, i ) => 
+        sortArr[i] = locations.find(({name}) => name === location)
+      )
+      setLocations(sortArr)
+      
+    }
+    
+
 
     useEffect(()=>{
         fetchData(`locations`)
         .then(respJson => setLocations(respJson))
         .catch(err => console.log(err))
+
+//         const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.sort())
+// console.log(fruits.reverse())
     }, [])
     return (
         <div className="locations">
           <h2>List of Locations</h2> 
+          <div className="buttons">
           <button
-          onClick={()=> setClicked(!clicked)}>{clicked ? 'Hide' : 'Show'} Locations</button>
+          onClick={()=> setClicked(!clicked)}>
+            {clicked ? 'Hide' : 'Show'} Locations
+            </button>
+
+            <button
+            onClick={() => sortBy()}>
+                Sort by Name
+            </button>
+
+            <button>
+                Sort by Climate
+            </button>
+
+            <button>
+                Sort by Terrain
+            </button>
+          </div>
+         
           <ul className="thumbnailGrid">
             {
                 clicked &&
