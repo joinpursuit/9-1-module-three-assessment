cf28ce4fb13e897366adc180ad76b1a1e19738bb
TrianePeart <TrianePeart@gmail.com>
2022-12-01 03:00:16 -0500

    Save Point - Looks cleaner but lost Sort

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,7 +1,6 @@
 import {useState, useEffect} from 'react';
 
 export default function Locations () {
-    //NOTE: This Version looks closer to the photo with the spacing still having left issue. Words are still smooshed. Might still be the Li as believed. Button still in poor placement. May also be CSS issue. Try 3rd attempt and if still happening mess with css before splitting.
     const [datas, setData] = useState([])
     const [hide, setHide] = useState(false)
 
@@ -18,84 +17,52 @@ export default function Locations () {
         setHide(!hide)
     }
 
-    // function handleChange(){
-    //     const findLocations = datas.map(({id, name, climate, terrain}) => {
-    //         //This isnt calling the array
-    //         return (
-    //             <div className='test'>
-    //             <ul key={id} >
-    //                 <li>
-    //                     <ul>
-    //                         <li><span>Name: </span>{name}</li>
-                       
-    //                         <li><span>Climate: </span>{climate}</li>
-                        
-    //                         <li><span>Terrain: </span>{terrain}</li>
-    //                     </ul>
-    //                     {/* css seems to want this but it looks very ugly */}
-    //                 </li>
-    //                 {/* Note: Even if this works its ugly. Go back to practice for better practice example */}
-    //             </ul>
-    //             </div>
-    //         )
-    //     })
-    //     setLocation(findLocations)
-    //     hide ? setHide(false) : setHide(true)
-    //     //might need curly??? 
-    // }
-    //This is meant to pull up locations if it works. 
-
-    function handleSort(e){
-        //Ask how to do this with a Tenarary
+    function handleSort(){
         const sort = datas.sort(function(a, b){
-            if(a[e] > b[e]){
+            if(a > b){
                 return 1
-                //see if works when removing curly after refactor 
-                //see if works without returns
-            }else if (a[e] < b[e]){
+            }else if (a < b){
                 return -1
             }else{
                 return 0
             }
         })
         setData(sort)
-        // setHide(true)
     };
-   //NOTE: ISSUE MIGHT BE THE SORT CHOICE
-
     return (
         <main className='locations'>
             <h2>List of Locations</h2>
             <button type='submit' onClick={hideLocations}>
                 {!hide ? 'Show Locations' : 'Hide Locations'}
             </button>
-            {/* Note buttons appearing under each other and not next to each other. Ask if Css issue or overcoding issue*/}
             {hide ? (
                 <div>
                    <button type='submit' onClick={() => handleSort('name')}>SORT BY NAME</button>
-                   {/* Note ask why handle needed the param if I already made function above */}
                    <button type='submit' onClick={() => handleSort('climate')}>SORT BY CLIMATE</button>
                    <button type='submit' onClick={() => handleSort('terrain')}>SORT BY TERRAIN</button>
                    <ul>
-                    {datas.map(({ name, climate, terrain}) =>{
+                    {datas.map(({id, name, climate, terrain}) =>{
                         return (
+                            <ul key={id}>
                             <li>
-                                <ul>
-                                    <li>
-                                    <li><span>Name: </span>{name}</li>
-                                    <br/>
-                                    <li><span>Climate: </span>{climate}</li>
-                                           
-                                    <li><span>Terrain: </span>{terrain}</li>
-                                    </li>
-                                </ul>
+                              <ul>
+                                <li>
+                                  <span>Name: </span> {name}
+                                </li>
+                                <li>
+                                  <span>Climate: </span> {climate}
+                                </li>
+                                <li>
+                                  <span>Terrain: </span> {terrain}
+                                </li>
+                              </ul>
                             </li>
+                          </ul>
                         )
                     })}
                    </ul>
                 </div>
             ): null}
-            {/* <div>{hide && location}</div> */}
         </main>
     );
 };
