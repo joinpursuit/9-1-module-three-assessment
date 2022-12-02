7170908f65f12e7c58319b1cb4b2c70856d81196
PESolut <jahaadpetty@pursuit.org>
2022-12-02 09:18:13 -0500

    Movies Page - Started

--- src/Movies.js
+++ src/Movies.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 
 const Movies = () => {
+    const [open, setOpen] = useState(false)
+
+    const handleOpen = () => {
+        setOpen(!open)
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <h1>Select a Movie</h1>
+        <button onClick={handleOpen}>Dropdown</button>
+        {open ? (
+            <ul className="menu">
+                <li clasName='menu-item'>
+                    <button>example 1</button>
+                </li>
+                <li clasName='menu-item'>
+                    <button>example 2</button>
+                </li>
+            </ul>
+        ) : null }
+        {open ? <div>Is Open</div> : <div>Is Closed</div>}  
+        </>
     );
 };
 
