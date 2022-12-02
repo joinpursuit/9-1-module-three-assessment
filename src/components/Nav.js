e253d9d5200504834823f08131b78d23231a5b2d
Lennie Nurse <lnursej@gmail.com>
2022-12-01 23:10:26 -0500

    "update"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,11 @@ export default function Nav() {
   return (
     <nav className="Navbar">
       <Link to="/">
-        <img src="" alt="" />
+        <img
+          style={{ height: "50px", width: "50px" }}
+          src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/wOBU3SLjQ9358Km9YWYasPZyebp.jpg"
+          alt="The Red turtle"
+        />
       </Link>
       <Link to="movies">Movies</Link>
       <Link to="people">People</Link>
