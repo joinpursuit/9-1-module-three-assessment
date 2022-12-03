7d32748a309d7cd32e471ec0b57388edcb8360f9
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-03 16:14:40 -0500

    Locations updated

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -2,20 +2,20 @@ import React from "react";
 import { useState } from "react";
 import locations from "../data/locations.json";
 
-console.log(locations);
-
 function Locations() {
   const [showLocation, setShowLocation] = useState(false);
 
   function handleOnClick() {
     setShowLocation(!showLocation);
   }
-  const loactionsList = locations.map((location) => {
-    <li>
-      <p>{location.name}</p>
-      <p>{location.climate}</p>
-      <p>{location.terrain}</p>
-    </li>;
+  const locationsList = locations.map((location) => {
+    return (
+      <li>
+        <p>{location?.name}</p>
+        <p>{location?.climate}</p>
+        <p>{location?.terrain}</p>
+      </li>
+    );
   });
 
   return (
@@ -24,7 +24,7 @@ function Locations() {
       <button onClick={handleOnClick}>
         {showLocation ? "Hide Location" : "Show Location"}
       </button>
-      <ul></ul>
+      <ul>{locationsList}</ul>
     </div>
   );
 }
