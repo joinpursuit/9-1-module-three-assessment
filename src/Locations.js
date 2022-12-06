2e2d06cff036a70de45bb147451348626d6ad220
PESolut <jahaadpetty@pursuit.org>
2022-12-06 14:34:25 -0500

    Locations.js - show locations button on screen

--- src/Locations.js
+++ src/Locations.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from "react"
 
 const Locations = () => {
+    const [renderLocationsButton, setRenderLocationsButton] = useState(false)
+    
+    const handleSubmitRLB = (event)=> {
+        event.preventDefault()
+        renderLocationsButton ? setRenderLocationsButton(false) : setRenderLocationsButton(true)
+        // console.log('Locations.js - handleSubmitRLB function - boolean set to: ',{renderLocationsButton})
+    }
+
+
+
     return (
         <>
+        <div className='locations'>
+            <h2>List of Locations</h2>
+
+            <form onSubmit={handleSubmitRLB}>
+                <button type='submit'>
+                    Show Locations
+                </button>
+            </form>
             
+        </div>
         </>
     );
 };
