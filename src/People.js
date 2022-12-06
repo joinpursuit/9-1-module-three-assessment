f07f33b358dd29ee0417eb003a3d293c4f0e8b59
PESolut <jahaadpetty@pursuit.org>
2022-12-06 06:57:08 -0500

    People.js - search function completed

--- src/People.js
+++ src/People.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import Select from 'react-select';
 import { useState, useEffect } from "react";
 
-const People = () => {
-    const [formStateValue, setFormStateValue] = useState ("")
+import PeopleRender from './PeopleRender';
 
-    const searchPersonData = event => {
+const Test = () => {
+    const [ searchBox, setSearchBox ] = useState("")
+    const [ peopleData, setPeopleData ] = useState([])
+    const [ personObject, setPersonObject ] = useState({})
+    const [ movieRenderCondition, setMovieRenderCondition ] = useState(false)
+
+    useEffect(() => {
+        console.log(searchBox)
+        fetch('people.json')
+        .then(response => response.json())
+        .then(data => {
+            setPeopleData(data)
+        })
+
+    },peopleData)
+
+
+    const handleSubmit = (event) => {
         event.preventDefault()
-        console.log ('searchPersonData function, form selection: ',event.label)
+        console.log("handleSubmit function, form submission: ", searchBox )
+
+        fetch('people.json')
+        .then(response => response.json())
+        .then(data => {
+            setPeopleData(data)
+        })
+        console.log("handleSubmit function, people object: ", peopleData )
+        
+        // find the iteration of people array of objects that matches input name
+        const result = peopleData.find(({ name }) => name.toLowerCase() === searchBox.toLowerCase())
+        console.log("handleSubmit function, found person object: ",result)
+        // render
+        setMovieRenderCondition(true)
+        setPersonObject(result)
     }
 
     return (
+        <>
         <div className='people'>
-            <h2>Search for a person</h2>
-            <form>
-                <label>
-                    <input type="text" onChange={searchPersonData} value={formStateValue}/>
+            <h2>Search for a Person</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Enter your query:
+                    <input 
+                    type="text" 
+                    value={searchBox}
+                    onChange={(e) => setSearchBox(e.target.value)}/>
                 </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <button type="submit">Submit</button>
+        </form>
+        <aside>
+        {movieRenderCondition ? <PeopleRender person={personObject} /> : null}
+        </aside>
         </div>
+            
+            {/* {movieRenderCondition ? <PeopleRender person={personObject} /> : null} */}
+        </>
     );
 };
 
-export default People;
+export default Test;
